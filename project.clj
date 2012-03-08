5cbc54c6cb5370ded76d0ee20d4e721b812709ac
Arthur Edelstein <arthuredelstein@gmail.com>
2012-03-07 23:45:57 -0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-inspector "0.0.1"
+(defproject clj-inspector "0.0.2"
   :description "a small library for getting
                 var information from clojure source code"
   :dependencies [[org.clojure/clojure "1.3.0"]])
